4f2ed0fbc587fa61813626d69d2bd9f665ef24b7
Siri <75486736@qq.com>
2019-03-17 16:46:31 +0800

    修改类名

---
+++ HomeProject/HomeProject/ProjectDirectory/ExternalDocking/HomeDockingHeader.h
@@ -0,0 +1,14 @@
+//
+//  HomeDockingHeader.h
+//  HomeProject
+//
+//  Created by Siri on 2019/3/17.
+//  Copyright © 2019年 Siri. All rights reserved.
+//
+
+#ifndef HomeDockingHeader_h
+#define HomeDockingHeader_h
+
+#import "HomeViewController.h"
+
+#endif /* HomeDockingHeader_h */
