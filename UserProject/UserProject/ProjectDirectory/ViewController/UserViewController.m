97183fad69b80a1ab86275893538f82ac2455cf2
Siri <75486736@qq.com>
2019-03-20 16:52:11 +0800

    ...

--- UserProject/UserProject/ProjectDirectory/ViewController/UserViewController.m
+++ UserProject/UserProject/ProjectDirectory/ViewController/UserViewController.m
@@ -40,49 +40,6 @@
 {
     [self dismissModalViewControllerAnimated:YES];
 }
-
--(void)shareConten{
-     NSArray* imageArray = @[[UIImage imageNamed:@"head.png"]];
-    if (imageArray) {
-        
-        NSMutableDictionary *shareParams = [NSMutableDictionary dictionary];
-        [shareParams SSDKSetupShareParamsByText:@"分享内容"
-                                         images:imageArray
-                                            url:[NSURL URLWithString:@"http://mob.com"]
-                                          title:@"分享标题"
-                                           type:SSDKContentTypeAuto];
-        
-                       
-        //大家请注意：4.1.2版本开始因为UI重构了下，所以这个弹出分享菜单的接口有点改变，如果集成的是4.1.2以及以后版本，如下调用：
-        [ShareSDK showShareActionSheet:nil customItems:nil shareParams:shareParams sheetConfiguration:nil onStateChanged:^(SSDKResponseState state, SSDKPlatformType platformType, NSDictionary *userData, SSDKContentEntity *contentEntity, NSError *error, BOOL end) {
-                           switch (state) {
-                               case SSDKResponseStateSuccess:
-                               {
-                                   UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"分享成功"
-                                                                                       message:nil
-                                                                                      delegate:nil
-                                                                             cancelButtonTitle:@"确定"
-                                                                             otherButtonTitles:nil];
-                                   [alertView show];
-                                   break;
-                               }
-                               case SSDKResponseStateFail:
-                               {
-                                   UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"分享失败"
-                                                                                   message:[NSString stringWithFormat:@"%@",error]
-                                                                                  delegate:nil
-                                                                         cancelButtonTitle:@"OK"
-                                                                         otherButtonTitles:nil, nil];
-                                   [alert show];
-                                   break;
-                               }
-                               default:
-                                   break;
-                           }
-                       }];
-                }
-}
-
 -(void)shareCase2{
     
      NSArray* imageArray = @[[UIImage imageNamed:@"head.png"]];
@@ -90,12 +47,12 @@
     NSString *url = @"http://mob.com";
     NSString *title= @"分享标题";
     if (imageArray) {
+       
         [SSUIShareActionSheetStyle setActionSheetColor:[UIColor lightGrayColor]];
-        [SSUIShareActionSheetStyle setActionSheetBackgroundColor:[UIColor colorWithRed:0/255.0 green:0/255.0 blue:0/255.0 alpha:0.8]];
+        [SSUIShareActionSheetStyle setActionSheetBackgroundColor: [[UIColor whiteColor] colorWithAlphaComponent:0.8]];
         [SSUIShareActionSheetStyle setCancelButtonBackgroundColor:[UIColor lightGrayColor]];
         [SSUIShareActionSheetStyle setCancelButtonLabelColor:[UIColor blackColor]];
         
-        //NSString *encodedString = (NSString *)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault, (CFStringRef)shareModel.url, (CFStringRef)@"!$&'()*+,-./:;=?@_~%#[]", NULL, kCFStringEncodingUTF8));
         NSMutableDictionary *shareParams = [NSMutableDictionary dictionary];
         [shareParams SSDKSetupShareParamsByText:text
                                          images:imageArray
@@ -115,7 +72,7 @@
 -(void)payBtnClick{
    
     //[self presentViewController:[[MBMediator share] backHomeView:@{@"VCName":@"Home"}] animated:YES completion:nil];
-    [self shareConten];
+    [self shareCase2];
 }
 
 
